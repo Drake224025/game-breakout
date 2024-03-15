e08cf1068f41ef0bf991c6dea37da70b045f8d7d
Drake224025 <sasithamare@gmail.com>
2024-03-16 02:50:14 +0530

    Update app,js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ let ballCurrentPosition = ballStart;
 let timerId;
 let xDirection = 2;
 let yDirection = 2;
+let score = 0;
 
 // Create Blocks
 class Block {
@@ -114,12 +115,45 @@ timerId = setInterval(moveBall, 30);
 
 // check for collisions
 function checkForCollisions() {
-  // check for wall collisions
+  // check for block collisions
+  for (let i = 0; i < blocks.length; i++) {
+    if (
+      ballCurrentPosition[0] > blocks[i].bottomLeft[0] &&
+      ballCurrentPosition[0] < blocks[i].bottomRight[0] &&
+      ballCurrentPosition[1] + ballDiameter > blocks[i].bottomLeft[1] &&
+      ballCurrentPosition[1] < blocks[i].topLeft[1]
+    ) {
+      const allBlocks = Array.from(document.querySelectorAll(".block"));
+      allBlocks[i].classList.remove("block");
+      blocks.splice(i, 1);
+      changeDirection();
+      score++;
+      scoreDisplay.innerHTML = score;
 
+      // check for win
+      if (blocks.length === 0) {
+        scoreDisplay.innerHTML = "YOU WIN";
+        clearInterval(timerId);
+        document.removeEventListener("keydown", moveUser);
+      }
+    }
+  }
+
+  // check for wall collisions
   if (
     ballCurrentPosition[0] >= boardWidth - ballDiameter ||
     ballCurrentPosition[1] >= boardHeight - ballDiameter ||
     ballCurrentPosition[0] <= 0
+  ) {
+    changeDirection();
+  }
+
+  // check for user collisions
+  if (
+    ballCurrentPosition[0] > currentPosition[0] &&
+    ballCurrentPosition[0] < currentPosition[0] + blockWidth &&
+    ballCurrentPosition[1] > currentPosition[1] &&
+    ballCurrentPosition[1] < currentPosition[1] + blockHeight
   ) {
     changeDirection();
   }
